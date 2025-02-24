efba8849812a63fe554b9843cc87d31aa59dba28
hayashigeen <jonsontokeshi2458@gmail.com>
2025-02-25 08:48:54 +0900

    プロフィール設定の修正

--- public/js/app.js
+++ public/js/app.js
@@ -530,7 +530,7 @@ const modalForm = () => {
                     </button>
                 </h2>
                 <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
+                    <form class="accordion-body">
                         <div class="mb-3">
                             <div class="form-floating">
                                 <input type="password" class="form-control position-relative" id="currentPassword" placeholder="Current password">
@@ -541,21 +541,31 @@ const modalForm = () => {
                             </div>
                             <div class="input-error-text">test</div>
                         </div>
-                        <div class="form-floating mb-3">
-                            <input type="password" class="form-control position-relative" id="newPassword" placeholder="New password">
-                            <div class="pass-word-eye position-absolute top-50 end-0 translate-middle">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 612" width="15" height="15" style="fill: #566d83;"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"/></svg>
+                        <div class="mb-3">
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control position-relative" id="newPassword" placeholder="New password">
+                                <div class="pass-word-eye position-absolute top-50 end-0 translate-middle">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 612" width="15" height="15" style="fill: #566d83;"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"/></svg>
+                                </div>
+                                <label for="newPassword">新しいパスワード</label>
                             </div>
-                            <label for="newPassword">新しいパスワード</label>
+                            <div class="input-error-text">test</div>
                         </div>
-                        <div class="form-floating">
-                            <input type="password" class="form-control position-relative" id="confirmNewPassword" placeholder=" Confirm new password">
-                            <div class="pass-word-eye position-absolute top-50 end-0 translate-middle">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 612" width="15" height="15" style="fill: #566d83;"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"/></svg>
+                        <div class="mb-3">
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control position-relative" id="confirmNewPassword" placeholder=" Confirm new password">
+                                <div class="pass-word-eye position-absolute top-50 end-0 translate-middle">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 612" width="15" height="15" style="fill: #566d83;"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"/></svg>
+                                </div>
+                                <label for="confirmNewPassword">新しいパスワードを再入力</label>
                             </div>
-                            <label for="confirmNewPassword">新しいパスワードを再入力</label>
+                            <div class="btn-group">
+                                <button type="submit" id="password-submit-button" class="btn btn-primary" data-bs-dismiss="modal">変更を保存</button>
+                                <button type="button" class="btn btn-secondary btnModalClose" data-bs-dismiss="modal">キャンセル</button>
+                            </div>
+                            <div class="input-error-text">test</div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -573,7 +583,9 @@ const modalForm = () => {
     const currentDescription = localStorage.getItem('userDescription') || userDescription;
     const currentName = localStorage.getItem('userName') || userName;
     const imageNameDescriptionSubmitButton = document.querySelector("#image-name-description-submit-button");
+    const passwordSubmitButton = document.querySelector("#password-submit-button");
     imageNameDescriptionSubmitButton.disabled = true; 
+    passwordSubmitButton.disabled = true; 
     document.querySelector("#profileImage").src = currentImage;
     document.querySelector('#description').textContent = currentDescription;
     document.querySelector('#editUserName').value = currentName;
@@ -608,22 +620,22 @@ const modalForm = () => {
         }
     };
     const validateInputsPassword = () => {
-        const currentPassword = document.querySelector("#currentPassword");
-        const newPassword = document.querySelector("#newPassword");
-        const confirmNewPassword = document.querySelector("#confirmNewPassword");
-        if (currentPassword === 'ss'  && newDescription === currentDescription && newImage === undefined) {
-            imageNameDescriptionSubmitButton.disabled = true;
+        const currentPassword = document.querySelector("#currentPassword").value;
+        const newPassword = document.querySelector("#newPassword").value;
+        const confirmNewPassword = document.querySelector("#confirmNewPassword").value;
+        if (currentPassword !== '' && newPassword !== '' && confirmNewPassword !== '') {
+            passwordSubmitButton.disabled = false;
         } else {
-            imageNameDescriptionSubmitButton.disabled = false;
+            passwordSubmitButton.disabled = true;
         }
     }
     setTimeout(() => {
         document.querySelector("#editUserName").addEventListener('input', validateInputs);
         document.querySelector("#description").addEventListener('input', validateInputs);
         document.querySelector("#formFile").addEventListener('change', validateInputs);
-        document.querySelector("#currentPassword").addEventListener('input', validateInputs);
-        document.querySelector("#newPassword").addEventListener('input', validateInputs);
-        document.querySelector("#confirmNewPassword").addEventListener('input', validateInputs);
+        document.querySelector("#currentPassword").addEventListener('input', validateInputsPassword);
+        document.querySelector("#newPassword").addEventListener('input', validateInputsPassword);
+        document.querySelector("#confirmNewPassword").addEventListener('input', validateInputsPassword);
         document.querySelector("#editUserName").addEventListener('focusout', function() {
             const inputData = document.querySelector("#editUserName");
             if (inputData.value === '') {
@@ -769,7 +781,7 @@ function saveUserInfo(userID, newData) {
 }
 
 function checkPaswordEdit(currentPassword, newPassword, confirmNewPassword) {
-    if (currentPassword !== '' || newPassword !== '' || confirmNewPassword !== '') {
+    if (currentPassword !== '' || newPassword !== '' || confirmNewPassword !== '') { //(currentPassword === localStorage.getItem('userPassword') && newPassword !== '' && confirmNewPassword !== '' && newPassword === confirmNewPassword)
         const inputErrorText = document.querySelectorAll('.input-error-text');
         const formFloating = document.querySelectorAll('.form-floating');
         //現在のパスワードが間違っていた場合
